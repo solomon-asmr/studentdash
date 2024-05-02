8abcbbd6005685144cc3994a34dae59d91130250
solomon-asmr <selemonasmr7@gmail.com>
2024-05-03 00:08:48 +0300

    implemented dashboard ui frontend

--- frontend/dashboard/src/App.js
+++ frontend/dashboard/src/App.js
@@ -3,7 +3,18 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+import NavigationBar from './NavigationBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SubjectCard from './SubjectCard';
+import CourseDetails from './CourseDetails';
+import ToDoList from './ToDoList';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 function App() {
 
     axios.defaults.baseURL = 'http://localhost/local/studentdash';
@@ -30,6 +41,18 @@ function App() {
         <>
 
             <Dashboard studentInfo={studentInfo} courses={null}/>
+            <Router>
+                <Container className="d-flex justify-content-center" style={{ backgroundColor: '#1f4e79' }}>
+                    .
+                    <Routes>
+                        <Route path="/" element={<SubjectCard/>} />
+                        <Route path="/details" element={<CourseDetails/>} />
+                        <Route path="/back" element={<SubjectCard/>} />
+                        <Route path="/ToDo" element={<ToDoList/>} />
+                    </Routes>
+
+                </Container>
+            </Router>
 
         </>
     );
