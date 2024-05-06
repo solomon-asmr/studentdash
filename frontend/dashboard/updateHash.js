a7cb06bdee93b7f45fd317fe405b4d67bcd5e1a8
Or Bar Califa <orbarkalifa@gmail.com>
2024-05-06 21:18:24 +0300

    added automation for updating js and css hash names

--- frontend/dashboard/updateHash.js
+++ frontend/dashboard/updateHash.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const cssFileName = fs.readdirSync(`./build/static/css`)[0];
 const jsFileName = fs.readdirSync(`./build/static/js`)[0];
 const templateURL = 'C:/Users/orbar/Desktop/exlab/StudentDash/xampp/server/moodle/local/studentdash/templates/dashboard.mustache';
-let html = fs.readFileSync(templateURL, 'utf8');
+let template = fs.readFileSync(templateURL, 'utf8');
 
-html = html.replace(/\/static\/css\/main\..*?\.css/, `/static/css/${cssFileName}`);
-html = html.replace(/\/static\/js\/main\..*?\.js/, `/static/js/${jsFileName}`);
+template = template.replace(/\/static\/css\/main\..*?\.css/, `/static/css/${cssFileName}`);
+template = template.replace(/\/static\/js\/main\..*?\.js/, `/static/js/${jsFileName}`);
 
-fs.writeFileSync(templateURL, html);
+fs.writeFileSync(templateURL, template);
 
