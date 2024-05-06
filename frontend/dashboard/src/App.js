b379f0063a9b8818985b8c7cb46ce5d2a580522a
Or Bar Califa <orbarkalifa@gmail.com>
2024-05-06 18:30:58 +0300

    minor fixes

--- frontend/dashboard/src/App.js
+++ frontend/dashboard/src/App.js
@@ -1,9 +1,5 @@
-import Dashboard from "./Components/Dashboard";
 import {useEffect, useState} from "react";
 import axios from "axios";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import NavigationBar from './NavigationBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SubjectCard from './SubjectCard';
 import CourseDetails from './CourseDetails';
@@ -12,20 +8,17 @@ import {
     BrowserRouter as Router,
     Routes,
     Route,
-    Link
 } from 'react-router-dom';
 import {Container} from 'react-bootstrap';
 
 function App() {
-
-    axios.defaults.baseURL = 'http://localhost/local/studentdash';
 
     const [studentInfo, setStudentInfo] = useState({});
 
     // Function to fetch user data from the database
     const fetchUserData = async() => {
         try {
-            const response = await axios.get('/ajax/fetch_user_data.php'); // Replace with your API endpoint
+            const response = await axios.get('/ajax/fetch_data.php'); // Replace with your API endpoint
             console.log(response);
             setStudentInfo(response.data);
         } catch (error) {
@@ -40,20 +33,16 @@ function App() {
 
     return (
         <>
-
             <Router basename={'/local/studentdash/dashboard.php'}>
                 <Container className="d-flex justify-content-center" style={{backgroundColor: '#1f4e79'}}>
-                    .
                     <Routes>
                         <Route path="/" element={<SubjectCard/>}/>
                         <Route path="/details" element={<CourseDetails/>}/>
                         <Route path="/back" element={<SubjectCard/>}/>
                         <Route path="/ToDo" element={<ToDoList/>}/>
                     </Routes>
-
                 </Container>
             </Router>
-
         </>
     );
 }
