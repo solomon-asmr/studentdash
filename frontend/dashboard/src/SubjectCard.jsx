85c2978925c6d03250b60b6fa2fe4e697a93e4bf
Or Bar Califa <orbarkalifa@gmail.com>
2024-05-12 15:41:18 +0300

    started implementing dynamic data on front page

--- frontend/dashboard/src/SubjectCard.jsx
+++ frontend/dashboard/src/SubjectCard.jsx
@@ -8,8 +8,7 @@ function SubjectCard({studentInfo}) {
 
     function getDaysDifference(date1, date2) {
         const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
-        const diffDays = Math.round(Math.abs((date1 - date2) / oneDay));
-        return diffDays;
+        return Math.round(Math.abs((date1 - date2) / oneDay));
     }
 
     return (
