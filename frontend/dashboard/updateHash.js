f49bc3f07f597c3466ba6055a510096ad2358769
Or Bar Califa <orbarkalifa@gmail.com>
2024-05-17 16:20:34 +0300

    improved SubjectCard.jsx

--- frontend/dashboard/updateHash.js
+++ frontend/dashboard/updateHash.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const cssFileName = fs.readdirSync(`./build/static/css`)[0];
 const jsFileName = fs.readdirSync(`./build/static/js`)[0];
-const templateURL = 'C:/Users/97253/OneDrive/Desktop/StudentDash/MoodleWindowsInstaller-latest-403/server/moodle/local/studentdash/templates/dashboard.mustache';
+const templateURL = 'C:/Users/orbar/Desktop/exlab/StudentDash/xampp/server/moodle/local/studentdash/templates/dashboard.mustache';
 let template = fs.readFileSync(templateURL, 'utf8');
 
 template = template.replace(/\/static\/css\/main\..*?\.css/, `/static/css/${cssFileName}`);
