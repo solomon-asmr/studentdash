39f5ec1d90dd16505198eef30c9dfbbe5cd4711f
Or Bar Califa <orbarkalifa@gmail.com>
2024-05-19 19:07:57 +0300

    dynamic data into header todo's: -average needs to be calculated by points (נק"ז) -academic year -major (מגמה)

--- frontend/dashboard/src/App.js
+++ frontend/dashboard/src/App.js
@@ -34,16 +34,16 @@ function App() {
     return (
         <Container style={{backgroundColor: '#1f4e79'}}>
             <Router basename={'/local/studentdash/dashboard.php'}>
-                <NavigationBar studentInfo={studentInfo}/>
-                    <Routes>
-                        {/*<Route path="/" element={<NavigationBar studentInfo={studentInfo}/>}/>*/}
-                        <Route path="/" element={isLoading ? <Spinner animation="border"/> : studentInfo ?
-                            <SubjectCard studentInfo={studentInfo}/> : <div>No data available</div>}/>
-                        <Route path="/details/:courseId" element={<CourseDetails studentInfo={studentInfo} />} />
-                        <Route path="/back"
-                               element={studentInfo ? <SubjectCard studentInfo={studentInfo}/> : <div>No data available</div>}/>
-                        <Route path="/ToDo" element={<ToDoList studentInfo={studentInfo}/>}/>
-                    </Routes>
+                <NavigationBar studentInfo={studentInfo ? studentInfo : <div>No data available</div>}/>
+                <Routes>
+                    {/*<Route path="/" element={<NavigationBar studentInfo={studentInfo}/>}/>*/}
+                    <Route path="/" element={isLoading ? <Spinner animation="border"/> : studentInfo ?
+                        <SubjectCard studentInfo={studentInfo}/> : <div>No data available</div>}/>
+                    <Route path="/details/:courseId" element={<CourseDetails studentInfo={studentInfo}/>}/>
+                    <Route path="/back"
+                           element={studentInfo ? <SubjectCard studentInfo={studentInfo}/> : <div>No data available</div>}/>
+                    <Route path="/ToDo" element={<ToDoList studentInfo={studentInfo}/>}/>
+                </Routes>
             </Router>
         </Container>
     );
